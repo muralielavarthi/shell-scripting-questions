8cb8efba6ff22a5328ea06e7ab9412dc29b22c19
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 07:50:24 +0530

    c1

---
+++ 10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter user name"
+read USERNAME
+echo "enter password"
+read PASSWORD
+#read -p PASSWORD (-p:password not visible in terminal)
+
+if [ $USERNAME=="admin" && $PASSWORD=="admin" ]
+then
+    echo "Login Success"
+else
+    echo "Wrong Login"
+fi
