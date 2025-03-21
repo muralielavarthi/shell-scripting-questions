ad637f5f072c7f87cd0aa1d255351979dd6b6b79
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 09:32:27 +0530

    c1

--- 23.sh
+++ 23.sh
@@ -6,17 +6,19 @@ read NUMBER_1
 echo "enter number 2"
 read NUMBER_2
 
-let $(($NUMBER_1 + 0))
-echo $?
 
-function VALIDATE()
-{
-    if [ $NUMBER_1 -eq $NUMBER_1 ] && [ $NUMBER_2 -eq $NUMBER_2 ]
-    then
-        SUM=$(($NUMBER_1 + $NUMBER_2))
+#In the context of input validation, we use let to check if the input can be 
+#successfully used in an arithmetic operation:
+let $(($NUMBER_1 + 0))
+EXIT_STATUS1=echo $?
+
+let $(($NUMBER_2 + 0))
+EXIT_STATUS2=echo $?
+
+if [ EXIT_STATUS1 -eq 0 ] && [ EXIT_STATUS2 -eq 0 ]
+then
+     SUM=$(($NUMBER_1 + $NUMBER_2))
         echo "SUM is : $SUM"
-    else
-        echo "enter valid numbers"
-    fi
-}
-VALIDATE $NUMBER_1 $NUMBER_2
+else
+    echo "enter valid integer numbers"
+fi
