4e6e358294d157da8c8c4ddcefb1c4a5055b0d69
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 08:51:11 +0530

    c1

--- 20.sh
+++ 20.sh
@@ -3,11 +3,11 @@
 NUMBER=1
 while [ $NUMBER -le 5 ]
 do
-    if [ $i -eq 3 ]
+    if [ $NUMBER -eq 3 ]
     then
         continue
     else
-        echo $i
+        echo $NUMBER
     fi
 NUMBER=$(($NUMBER+1))
 done
