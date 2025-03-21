6fe2101a74f575964ab4efdf0f33188700749d63
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 07:13:23 +0530

    c1

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DATE=$(date +"%d-%m-%y")
+TIME=$(date +"%H:%M:%S")
+
+echo "DATE is: $DATE"
+echo "TIME is: $TIME"
---
+++ 4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch devops.txt
+free -h >>devops.txt
+cat devops.txt
