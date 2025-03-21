2e07963ba27c6d802c73dff32fda890be750c707
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 08:42:46 +0530

    c1

--- 17.sh
+++ 17.sh
@@ -4,5 +4,5 @@ NUMBER=1
 while [ $NUMBER -le 10 ]
 do
     echo $NUMBER
-NUMBER=$(($NUMBER+1))
+NUMBER=$(($NUMBER+1))  #incrementing 
 done
---
+++ 18.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true  #sets up an infinite loop
+do
+    sleep 1
+done
