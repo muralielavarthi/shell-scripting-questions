885bfd69e7e54fff55eb56f6a277c07ff02bb6db
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 09:33:46 +0530

    c1

--- 23.sh
+++ 23.sh
@@ -10,12 +10,12 @@ read NUMBER_2
 #In the context of input validation, we use let to check if the input can be 
 #successfully used in an arithmetic operation:
 let $(($NUMBER_1 + 0))
-EXIT_STATUS1=echo $?
+EXIT_STATUS1=$(echo $?)
 
 let $(($NUMBER_2 + 0))
-EXIT_STATUS2=echo $?
+EXIT_STATUS2=$(echo $?)
 
-if [ EXIT_STATUS1 -eq 0 ] && [ EXIT_STATUS2 -eq 0 ]
+if [ $EXIT_STATUS1 -eq 0 ] && [ $EXIT_STATUS2 -eq 0 ]
 then
      SUM=$(($NUMBER_1 + $NUMBER_2))
         echo "SUM is : $SUM"
