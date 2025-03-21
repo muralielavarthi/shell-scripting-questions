bdf567ab325d46ac7c52d3d240fa7859c91951bd
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 07:32:11 +0530

    c1

---
+++ 6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DATE=$(date +"%d-%m-%y")
+
+if [ $(($DATE%2)) -eq 0 ]
+then
+    echo "Even Day"
+else
+    echo "Not Even Day"
+fi
+
+#$((...)) syntax to evaluate and return the result. Without $((...)), 
+#Bash would treat the expression as a string and not perform arithmetic correctly.
