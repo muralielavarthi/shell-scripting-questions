99cb852f288449d3de9353a989921178a44f8e64
Murali Elavarthi <muralis2p@gmail.com>
2025-03-21 09:12:08 +0530

    c1

---
+++ 23.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter number 1"
+read NUMBER_1
+
+echo "enter number 2"
+read NUMBER_2
+
+SUM=$(($NUMBER_1 + $NUMBER_2))
+echo "SUM is : $SUM"
---
+++ 24.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter number 1"
+read NUMBER_1
+
+echo "enter number 2"
+read NUMBER_2
+
+MUL=$(($NUMBER_1 * $NUMBER_2))
+echo "MUL is : $MUL"
