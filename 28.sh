d3eeb06ce20212723b728af72b494bbab87efdf0
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 08:18:39 +0530

    c2

--- 28.sh
+++ 28.sh
@@ -6,6 +6,7 @@ if [ $CURRENT_DATE -lt 10 ]
 then
     echo "Current date Less than 10"
 elif [ $CURRENT_DATE -eq 10 ]
+then
     echo "Current date Equals to 10"
 else
     echo "current date Greater than 10"
