a747e6e39c0c2eaff220834b5acbc138a0294778
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 09:42:47 +0530

    c2

--- 32.sh
+++ 32.sh
@@ -4,4 +4,4 @@ read STRING
 SUB_STRING=${STRING:2:9}  # no need of $to access STRING value
 echo $SUB_STRING
 
-#{string:start_index:total_length} - space is included in length
+#{string-variable:start_index:total_length} - space is included in length
