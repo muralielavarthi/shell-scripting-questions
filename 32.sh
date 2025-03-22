618054e2763dca102e1977e7f9d07cda66ada14c
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 09:41:51 +0530

    c2

--- 32.sh
+++ 32.sh
@@ -1,7 +1,7 @@
 echo -n "enter the string:"  # -n: no new line, take input in same line, by default echo adds new line
 read STRING
 
-SUB_STRING=${$STRING:2:9}
+SUB_STRING=${STRING:2:9}  # no need of $to access STRING value
 echo $SUB_STRING
 
 #{string:start_index:total_length} - space is included in length
