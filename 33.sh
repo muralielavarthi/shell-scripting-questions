573653b1c37e014cb9ec50bd729e284912fa875a
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 10:08:26 +0530

    c2

--- 33.sh
+++ 33.sh
@@ -3,23 +3,19 @@
 echo -n "enter valid directory path:"
 read DIR
 
-function ERROR_CHECK(){
 
+
+if [ -d $DIR ]
+then
     LOG_FILES= $(find $DIR -type f -name "*.log")
-
     while read LINE
     do
         grep "error" $LINE
         if [ $? -eq 0 ]
         then
-            $LINE >>output.txt
+            $LINE >> output.txt
         fi
-    done << $LOG_FILES
-}
-
-if [ -d $DIR ]
-then
-    ERROR_CHECK
+    done <<< $LOG_FILES   # when you want pass file as input
 else
     echo "enter valid directory path"
 
