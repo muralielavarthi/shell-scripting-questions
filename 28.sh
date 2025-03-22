66b3510c1ebc42ff5d0068fc677700d0966e7353
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 08:15:35 +0530

    c2

---
+++ 28.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DATE=$(date +"%d")
+
+if [ $CURRENT_DATE -lt 10 ]
+then
+    echo "Current date Less than 10"
+elif [ $CURRENT_DATE -eq 10 ]
+    echo "Current date Equals to 10"
+else
+    echo "current date Greater than 10"
+fi
