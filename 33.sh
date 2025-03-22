730f65050909e7f45d339f38cd03001159ada4c1
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 10:27:54 +0530

    c2

--- 33.sh
+++ 33.sh
@@ -3,15 +3,15 @@
 set -x
 echo -n "enter valid directory path:"
 read DIR
-if [ -d "$DIR" ]
+if [ -d $DIR ]
 then
     LOG_FILES=$(find "$DIR" -type f -name "*.log")
     while read LINE
     do
-        grep "error" "$LINE"
+        grep "error" $LINE
         if [ $? -eq 0 ]
         then
-            echo "$LINE" >> output.txt
+            echo $LINE >> output.txt
         fi
     done <<< $LOG_FILES   # when you want pass file as input
 else
