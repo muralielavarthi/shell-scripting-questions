4edad6fe75384c37939b8286303cb41bc7445d64
Murali Elavarthi <muralis2p@gmail.com>
2025-03-22 11:04:59 +0530

    c3

--- 33.sh
+++ 33.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-echo -n "Enter valid directory path: "
+echo -n "enter valid directory path:"
 read DIR
-
-if [ -d "$DIR" ]; then
+if [ -d $DIR ]
+then
     LOG_FILES=$(find "$DIR" -type f -name "*.log")
-    
-    # Create/clear output.txt before appending
-    > output.txt
-    
-    while read -r LINE; do
-        # Check if the script has permission to read the log file
-        if [ -r "$LINE" ]; then
-            # Check if the log file contains "error" and append to output.txt
-            grep -i "error" "$LINE" &>/dev/null
-            if [ $? -eq 0 ]; then
-                echo "$LINE" >> output.txt
+    while read LINE
+    do
+        if [ -r $LINE ]
+        then
+            grep -i "error" $LINE &>>log-file.txt
+            if [ $? -eq 0 ]
+            then
+                echo $LINE >>output.txt
             fi
         else
-            echo "Permission denied to read: $LINE" >> output.txt
-        fi
-    done <<< "$LOG_FILES"  # Pass files to the while loop
-
-    echo "Errors have been logged to output.txt"
+            echo "No read permisson $LINE"
+        fi  
+    done <<< $LOG_FILES   # when you want pass file as input (<<<)
 else
-    echo "Invalid directory path"
+    echo "enter valid directory path"
+
 fi
+
+cat output.txt
